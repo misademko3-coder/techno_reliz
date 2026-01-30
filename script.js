2d38e1a99dcf178b445841733d5807ba077773d0
Oleksandr <sashapylypenko.pov@gmail.com>
2026-01-30 15:23:39 +0200

    fix: hot fix

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("привіт зі світу ігор")
+let cart_buttons = document.querySelectorAll(".to-cart");
+
+cart_buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const id = button.parentElement.dataset.id;
+
+    alert(`Товар з ID ${id} додано до кошика!`);
+  });
+});
